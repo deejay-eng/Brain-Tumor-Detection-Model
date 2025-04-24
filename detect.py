3356ab9f8444596c186f818ddd60430480b262e5
kj <148381865+kaanchiiii@users.noreply.github.com>
2025-04-24 19:11:09 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -21,4 +21,6 @@ def detect(img_path, model, image_size=128):
         return result, confidence_score
 
     except Exception as e:
+        # Print detailed error to the console
+        print(f"Error in detect function: {e}")
         return "Error", 0.0
